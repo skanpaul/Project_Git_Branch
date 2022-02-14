94e3de3c713fd8d034286e894264a13b09743af7
Sorakannn SurfacePro7 <sorakann.ki@gmail.com>
2022-02-14 21:35:56 +0100

    je mange une fraise

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main(void)
     printf("Je mange une poire \n");
     printf("Je mange une pomme \n");
     printf("Je mange une cerise \n");
+    printf("Je mange une fraise \n");
 
     return 0;
 }
